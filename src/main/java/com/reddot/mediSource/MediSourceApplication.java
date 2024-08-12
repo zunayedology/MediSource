838e16ab1db5ad01d3f54c79e33211d8cc3a688b
Zunayed <zunayedology@gmail.com>
2024-08-12 12:29:06 +0600

    Initial Commit

---
+++ src/main/java/com/reddot/mediSource/MediSourceApplication.java
@@ -0,0 +1,13 @@
+package com.reddot.mediSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediSourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reddot/mediSource/MediSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reddot.mediSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
